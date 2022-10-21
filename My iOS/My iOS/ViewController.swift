52241a5f42585f2c1f7f6a49a9b10795b1c56153
Duc Vu <vmd211099@gmail.com>
2022-10-21 13:53:00 +0700

    first commit

---
+++ My iOS/My iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My iOS
+//
+//  Created by duc.vu1 on 21/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+}
+
